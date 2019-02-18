fd8c51169e12e38b4aad755a7cbed514aff13877
Daniel <danieldin95@163.com>
2019-02-18 15:18:30 -0500

    add log support.

---
+++ lib/log.py
@@ -0,0 +1,25 @@
+'''
+Created on Feb 21, 2019
+
+@author: info
+'''
+
+import logging
+
+isConfig = False
+
+def basicConfig(filename='openlan.log', level=logging.DEBUG):
+    """
+    logging.debug("This is a debug log.")
+    logging.info("This is a info log.")
+    logging.warning("This is a warning log.")
+    logging.error("This is a error log.")
+    logging.critical("This is a critical log.")
+    """
+    global isConfg
+
+    if isConfig:
+        return
+
+    LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+    logging.basicConfig(filename = filename, level = level, format = LOG_FORMAT)
