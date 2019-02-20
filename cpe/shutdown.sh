b8735fd144b36dbfe3251eaa4d5e54ed937a4f05
Daniel <danieldin95@163.com>
2019-02-19 23:33:12 -0500

    aa

--- cpe/shutdown.sh
+++ cpe/shutdown.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#
-# NetworkManager on $ETH MUST disable and clear default gw. 
-#
-
 curdir=`dirname $0`
 
 cd `dirname $curdir` && python -m cpe.daemon -a stop
--- ope/shutdown.sh
+++ ope/shutdown.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#
-# NetworkManager on $ETH MUST disable and clear default gw. 
-#
-
 curdir=`dirname $0`
 
 cd `dirname $curdir` && python -m ope.daemon -a stop
