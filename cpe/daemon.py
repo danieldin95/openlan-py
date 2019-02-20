81be86659abda1aa05e9826a99e015fccf541061
Daniel <danieldin95@163.com>
2019-02-19 22:06:25 -0500

    add daemon support.

---
+++ cpe/daemon.py
@@ -0,0 +1,50 @@
+'''
+Created on Feb 23, 2019
+
+@author: info
+'''
+
+import logging
+
+from lib.daemon import Daemon
+from lib.log import basicConfig
+
+from options import addOptions
+from options import parseOptions
+
+from bridge import Bridge
+
+class CpeDaemon(Daemon):
+    """"""
+    def run(self):
+        """"""
+        opts, _ = parseOptions()
+
+        logging.info("starting {0}".format(self.__class__.__name__))
+
+        Bridge(opts.gateway, int(opts.port), maxsize=1514).loop()
+
+def main():
+    """"""
+    addOptions()
+
+    opts, _ = parseOptions()
+
+    if opts.verbose:
+        basicConfig(opts.log, logging.DEBUG)
+    else:
+        basicConfig(opts.log, logging.INFO)
+
+    daemon = CpeDaemon(pidfile=opts.pid)
+
+    if opts.action == 'start':
+        daemon.start()
+    elif opts.action == 'stop':
+        daemon.stop()
+    elif opts.action == 'restart':
+        daemon.restart()
+    else:
+        print daemon.status()
+
+if __name__ == '__main__':
+    main()
---
+++ cpe/options.py
@@ -0,0 +1,28 @@
+'''
+Created on Feb 23, 2019
+
+@author: info
+'''
+
+import optparse
+
+opt = optparse.OptionParser()
+
+def addOptions():
+    """"""
+    opt.add_option('-v', '--verbose', action="store_true", 
+                   dest='verbose', default=False, help='enable verbose')
+    opt.add_option('-g', '--gateway', action='store', 
+                   dest='gateway', default='openlan.net', help='the address of ope connect to')
+    opt.add_option('-p', '--port', action='store', 
+                   dest='port', default=10001, help='the port of ope connect to')
+    opt.add_option('-P', '--pid', action='store', 
+                   dest='pid', default='/var/run/cpe.pid', help='the file pid saved')
+    opt.add_option('-L', '--log', action='store', 
+                   dest='log', default='/var/log/cpe.log', help='the file log saved')
+    opt.add_option('-a', '--action', action="store", 
+                   dest='action', default='status', help='the action include start, stop, restart.')
+
+def parseOptions():
+    """"""
+    return opt.parse_args()
