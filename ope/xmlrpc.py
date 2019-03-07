7f0a65b56d59283ea05e3161a11bde7ad7fd1440
Daniel Ding <danieldin95@163.com>
2019-03-07 10:52:11 +0800

    Update xmlrpc.py

--- ope/xmlrpc.py
+++ ope/xmlrpc.py
@@ -7,35 +7,40 @@ from .gateway import Gateway
 
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 
-def hi(name):
-    """"""
-    return 'Hi {0}'.format(name)
-
-def listPort():
-    """"""
-    servers = []
-    for server in Gateway.listServer():
-        servers.append(server.port)
-
-    return servers
-
 class XmlRpcServer(object):
     """"""
 
-    def __init__(self, port=8000, addr="0.0.0.0"):
+    def __init__(self, port=8000, addr="0.0.0.0", **kws):
         """"""
         self.port = port
-        self.addr = addr
-        
+        self.addr = addr  
+        self.portmap = kws.get('portmap', [])
+   
         self.server = SimpleXMLRPCServer((self.addr, self.port))
-        self.server.register_function(hi, "hi")
-        self.server.register_function(listPort, "listPort")
+        self.register(self.hi, "hi")
+        self.register(self.listPort, "listPort")
   
-    def start(self, port=8000, addr="0.0.0.0"):
+    def start(self):
         """"""
         self.server.serve_forever()
   
     def register(self, func, name):
         """"""
         self.server.register_function(func, name)
+
+    def hi(self, name):
+        """"""
+        return 'Hi {0}'.format(name)
+
+    def listPort(self):
+        """"""
+        ports = []
+        for p in self.portmap:
+            port = p.get('openPort')
+            if port is None:
+                continue
+   
+            ports.append(port)
+
+        return ports
   
