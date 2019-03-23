949cd16ec0dd50441ee4660ede04385643034508
Daniel Ding <danieldin95@163.com>
2019-03-22 22:09:02 -0400

    suport multiprocess.

--- sky/sky/opectl.py
+++ sky/sky/opectl.py
@@ -4,35 +4,19 @@ Created on Feb 28, 2019
 @author: info
 '''
 
-import grpc
-import sys
-
-from ope import ope_pb2
-from ope import ope_pb2_grpc
-
-def getCpe(to='127.0.0.1:50051'):
-    """"""
-    cpes = []
-
-    with grpc.insecure_channel(to) as channel:
-        stub = ope_pb2_grpc.OpeStub(channel)
-        for cpe in stub.GetCpe(ope_pb2.CpeRequest(name='all')):
-            cpes.append(cpe)
-
-    return cpes
+import xmlrpclib
 
 class OpeCtl(object):
     """"""
-    def __init__(self, connect_to="127.0.0.1:50051"):
+    def __init__(self, connect_to="127.0.0.1:5851"):
         """"""
-        self.connect_to = connect_to
-        self.channel    = grpc.insecure_channel(self.connect_to)
-        self.stub       = ope_pb2_grpc.OpeStub(self.channel)
+        self.connectto = connect_to
+        self.rpcproxy   = xmlrpclib.ServerProxy("http://{0}/".format(self.connectto))
 
-    def getCpe(self):
+    def listCpe(self):
         """"""
-        return self.stub.GetCpe(ope_pb2.CpeRequest(host='all'))
-    
-    def getFib(self):
+        return self.rpcproxy.listCpe()
+
+    def listMac(self):
         """"""
-        return self.stub.GetFib(ope_pb2.FibRequest(host='all'))
+        return self.rpcproxy.listMac()
